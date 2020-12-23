1d0f8e4981ad1dc52ee1d312b47b8bfd81e2f729
Meredith Jones <mj19003@gmail.com>
2020-12-23 08:11:08 -0500

    Updated with Engineer and Employee tests passing

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+ const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -21,12 +21,13 @@ const { ENGINE_METHOD_RAND } = require("constants");
 //(using the correct classes as blueprints!)
 
 //Should I be using var, or const here?
-const questions = [
+var questions = [
     {
     type: "input",
     name: "role",
     message: "What is your position at the company?"
 },
+
 {
     type: "input",
     name: "name",
@@ -37,6 +38,7 @@ const questions = [
     name: "id",
     message: "What is your employee ID number?"
 },
+
 {
     type: "input",
     name: "email",
@@ -50,51 +52,113 @@ const questions = [
         }
     },
 }
+
+{ 
+    type: "list",
+    message: "What type of employee would you like to add?",
+    choices: ["Manager", "Engineer", "Intern"],
+    name: "role"
+  }
 ];
+//Create targeted quesitons for manager, engineer, and intern. 
+//For a manager ask for their office number 
+//For an engineer ask for their github user name 
+//For an intern ask for their school
 
-employee.method();
-manager.method();
-engineer.method();
-intern.method();   
+const managerQuestions = {
+  type: "input",
+  message: "What is the manager's office number?",
+  name: "number"
+};
 
-// inquirer.prompt(questions).then((answers) => {
-//     console.log(JSON.stringify(answers, null, '  '));
-//   });
+const engineerQuestions = {
+  type: "input",
+  message: "What is the engineer's github account name?",
+  name: "gitAcct"
+};
 
+const internQuestions = {
+  type: "input",
+  message: "What shcool is the intern attending?",
+  name: "schoolIntern"
+};
 
+//Find out if the user wants to continue entering empoloyees, or if they are finished. 
 
-// Setting up questions for managers, engineers and interns.
-// For manager, ask office number 
-// For engineer ask github user name 
-// For intern ask for school name 
-// const managerQuestions = [
-// {
-//     type: "input",
-//     message: "What is your manager's office number?",
-//     name: "number"
-//   },
-// ]
-//   const engineerQuestions = [
-//   {
-    // type: "input",
-//     message: "What is your engineer's github user name?",
-//     name: "gitUser"
-//   },
-//   ]
+const finalQuestion = {
+  type: "list",
+  message: "Would you like to add another team member?",
+  choices: ["yes", "no"],
+  name: "restart"
+};
 
-//   const internQuestions = [
-//   {
-//     type: "input",
-//     message: "What is your intern's school?",
-//     name: "schoolIntern"
-//   },
-//   ]
+//Create an array to hold the answers
+const employeesArr = [];
 
-//   //Add more employees
-// //   
+// Start inquirer
+async function init() {
+  const userResponse = await inquirer.prompt(questions);
+
+  //Holding the responses
+  const {name, id, email, role} = userResponse;
+
+ 
+  //Creating a new manager 
+  //Use the "employee" answers for all but the office number
+  if (role === "Manager") {
+    const officenumber = await inquirer.prompt(managerQuestions);
+    const officeNumber = officenumber.number;
+    //Creating a new employee with the manager info.
+    const employee = new Manager(name, id, email, officeNumber);
+    // push the manager constructor object into the employee array
+    employeeArray.push(employee);
+    //Creating an engineer
+    //need to use gitAcct response 
+  } else if (role === "Engineer") {
+    const gitHub = await inquirer.prompt(engineerQuestions);
+    const github = gitHub.gitAcct
+    const employee = new Engineer(name, id, email, github);
+    // push the engineer constructor object into the employeeArray
+    employeeArray.push(employee);
+    //Creating an intern
+  } else if (role === "Intern") {
+    const schoolObj = await inquirer.prompt(internQuestions);
+    const school = schoolObj.schoolIntern
+    const employee = new Intern(name, id, email, school);
+    // push the intern constructor object into employeeArray
+    employeeArray.push(employee);
+  };
+    // Ask them if they want to add another employee
+  const restartInquirer = await inquirer.prompt(finalQuestion);
+  const {restart} = restartInquirer;
+  if (restart === "yes") {
+      init();
+  } else {
+    console.log(employeeArray)
+    //when all employees are added, call the render function and pass into employeeArray 
+    //The HTML generated in HTMLrender will be store in the const teamfile
+    const teamfile = render(employeeArray);
+    //write the team.html and store it in the output path via outputPath. 
+    //Write the content from the HTML stored in teamfile
+    fs.writeFile(outputPath, teamfile, function(err){
+      if(err){
+        throw err;
+      }
+    });
+  };
+  
+};
+init();
+
+function writeToFile(fileName, data) {
+    console.log(fileName, data)
+     fs.writeFileSync(path.join(process.cwd(), fileName), data)
+  
+}
+
 
 //Calling the function to pass in all employee objects
-// const teamInfo = render(employeeArray);
+//const teamInfo = render(employeeArray);
 
 // After the user has input all employees desired, 
 //call the `render` function (required
@@ -119,16 +183,72 @@ intern.method();
 
 // function to initialize 
 function init() {
-    inquirer.prompt(input,list)
+    //ask mnanger questions
+    //with the answers create manager Obj
+    //then call a function that will ask (switch case)
+    //create engineer
+        //ask engineer prompts
+        //create engineer with answers
+    //create inter
+        //askin intern prompts
+        //create intern with answers
+    //or build our team
+    inquirer.prompt(managerQuestions)
     .then(data => { 
-        console.log(data);
-        var temp = html(data)
-        writeToFile("output/team.html", temp)
-      
-            
+        console.log(data)
+        const manager = new Manager(data.name, data.id, data.email)
+        console.log(manager);
+         //push it into an array    
     })
 }
 init();
+
+
+//create an array to hold all the responses.
+const employeeArray = [];
+
+//Function to start inquirer
+async function init() {
+  const userResponse = await inquirer.prompt(questions);
+  //Deconstruct reponses
+  const {name, id, email, role} = userResponse;
+  // If role is manager, ask for office number. 
+  //With that response back, then create a new manager with userResponse and officeNumber response like: const employee = new Manager(name, id, email, officeNumber);
+  if (role === "Manager") {
+    const officenumber = await inquirer.prompt(managerQuestions);
+    //Deconstructing nested object
+    const officeNumber = officenumber.number;
+    const employee = new Manager(name, id, email, officeNumber);
+    // push the manager constructor object into employeesArr
+    employeeArray.push(employee);
+    
+  } else if (role === "Engineer") {
+    const gitHub = await inquirer.prompt(engineerQuestions);
+    const github = gitHub.gitUser
+    const employee = new Engineer(name, id, email, github);
+    // push the engineer constructor object into employeesArr
+    employeeArray.push(employee);
+  } else if (role === "Intern") {
+    const schoolObj = await inquirer.prompt(internQuestions);
+    const school = schoolObj.schoolIntern
+    const employee = new Intern(name, id, email, school);
+    // push the intern constructor object into employeesArr
+    employeeArray.push(employee);
+  };
+    // Ask them if they want to add another employee
+  const restartInquirer = await inquirer.prompt(restartQuestion);
+  const {restart} = restartInquirer;
+  if (restart === "yes") {
+      init();
+  } else {
+    console.log(employeesArr)
+    //After adding employees, call render function and pass into it the employeesArr. 
+    //The HTML generated in HTMLrender will be store in the const teamfile
+    const teamfile = render(employeesArr);
+    //write the team.html and store it in the output path via outputPath. Write the content from the HTML stored in teamfile
+    fs.writeFile(outputPath, teamfile, function(err){
+      if(err){
+        throw err;
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
